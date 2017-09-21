cf198ef1b5d9a8b4a24f8f3615a1f9df6cea60fb
Wickerman2 <christian_wickerstrom@hotmail.com>
2017-09-21 15:58:02 +0200

    Anther commit

---
+++ ixdproject/Assets/BirdMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMovement : MonoBehaviour {
+
+    Vector3 velocity = Vector3.zero;
+    public Vector3 gravity;
+    public Vector3 flapVelocity; 
+    bool didFlap = false;
+    public float maxSpeed = 5f;
+    public float forwardSpeed = 1f;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // For Graphic & Input updates
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            didFlap = true;
+        }
+    }
+
+    // For physic engine updates
+    void FixedUpdate () {
+        velocity += gravity * Time.deltaTime;
+        velocity.x = forwardSpeed; 
+
+        if (didFlap == true)
+        {
+            didFlap = false;
+            if (velocity.y < 0)
+            {
+                velocity.y = 0;
+            }
+
+            velocity += flapVelocity;
+        }
+
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+
+        transform.position += velocity * Time.deltaTime;
+
+        // To make the bird angle downwards
+        float angle = 0;
+        if (velocity.y < 0)
+        {
+            angle = Mathf.Lerp(0, -90, -velocity.y / maxSpeed);
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+	}
+}
